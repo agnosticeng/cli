fc23ddd374b666ca81892a06e0a1bbbb6dec897f
Yann Amsellem <yann@agnostic.engineering>
2025-12-01 14:47:04 +0100

    feat(teams): implement team list and select cmd

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,9 +1,11 @@
 pub mod pipeline;
 pub mod project;
 pub mod system;
+pub mod team;
 pub mod user;
 
 pub use pipeline::{PipelineAction, handle_pipeline_command};
 pub use project::{ProjectAction, handle_project_command};
 pub use system::SystemAction;
+pub use team::TeamAction;
 pub use user::UserAction;
